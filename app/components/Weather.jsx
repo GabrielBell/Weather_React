0b57e927713c8ccea17ed6128b2240b07c640f0a
Gabriel Bell <gmbell23@gmail.com>
2017-04-14 18:30:55 -0400

    Added Error Modal for invalid cities

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -2,26 +2,32 @@ var React= require('react');
 var WeatherForm= require('WeatherForm');
 var WeatherMessage=require('WeatherMessage');
 var openWeatherMap= require('openWeatherMap');
+var ErrorModal= require('ErrorModal');
 
 var Weather= React.createClass({
 	getInitialState: function(){
 		return {
 			isLoading: false
+
 		}
 	},
 	handleSearch: function(location){
 		var that= this;
-		this.setState({	isLoading: true	});
+		this.setState({	
+			isLoading: true,
+			errorMessage: undefined	
+		});
+
 		openWeatherMap.getTemp(location).then(function(temp){
 			that.setState({	location: location,	temp: temp, isLoading: false});
 		}, function(err){
-			that.setState({	isLoading: false	});
-			alert('No City Found!');
+			that.setState({	isLoading: false, errorMessage:err.message	});
+			
 		});
 	
 	},
 	render: function(){
-		var {isLoading,temp,location}= this.state;
+		var {isLoading,temp,location,errorMessage}= this.state;
 		//need a way to conditionally load message based on isLoading
 		function renderMessage(){
 			if(isLoading){
@@ -30,11 +36,17 @@ var Weather= React.createClass({
 				return <WeatherMessage temp={temp} location={location}/>
 			}
 		}
+		function renderError(){
+			if(typeof errorMessage ==='string'){
+				return <ErrorModal message={errorMessage}/>
+			}
+		}
 		return(
 			<div>
 				<h1 className='text-center'>Get Weather</h1>
 				<WeatherForm onSearch={this.handleSearch}/>
 				{renderMessage()}
+				{renderError()}
 			</div>
 		);
 	}
