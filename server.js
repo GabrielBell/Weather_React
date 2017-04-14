ab27bb4de3f91eeb95ce2b43eed641510a004ae1
Gabriel Bell <gmbell23@gmail.com>
2017-04-14 16:09:08 -0400

    Update middleware for localhost

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ var app= express();
 
 const PORT= process.env.PORT || 8080;
 //add fix for weatherMap API call. redirect any https to http
+
 app.use(function(req,res,next){
-	if(req.headers['x-forwarded-proto']==='http'){
-		next();
-	}else{
+	if(req.headers['x-forwarded-proto']==='https'){
 		res.redirect('http://' + req.hostname + req.url)
+		
+	}else{
+		next();
 	}
 })
 app.use(express.static('public'));
